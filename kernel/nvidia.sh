16f00f551702ccb68d28a529ff5fc65260482454
Marco Lavorini <marlavo1973@gmail.com>
2023-06-05 19:44:02 +0200

    Updated to version 6.3.6

--- kernel/nvidia.sh
+++ kernel/nvidia.sh
@@ -4,13 +4,22 @@
 #
 set -e
 
-VERSION=6.3.5
+VERSION=6.3.6
 NVIDIA=${NVIDIA:-530.41.03}
+NUMJOBS=${NUMJOBS:-$(expr $(nproc) + 1)}
+
+set -e
+
+if [ ! $UID = 0 ]; then
+  echo You must be 'root' to execute this script!
+  exit 1
+fi
 
 # install nvidia modules
 if [ -d /usr/src/nvidia-$NVIDIA ]; then
   cd /usr/src/nvidia-$NVIDIA
   make clean
-  KERNEL_UNAME=$VERSION make modules
-  KERNEL_UNAME=$VERSION make modules_install
+  KERNEL_UNAME=$VERSION make -j$NUMJOBS  modules
+  KERNEL_UNAME=$VERSION make -j$NUMJOBS  modules_install
+  depmod -a
 fi
