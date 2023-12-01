98cbe13bc7524e60004681acb1a23e7346f81853
Marco Lavorini <marlavo1973@gmail.com>
2023-12-01 18:00:39 +0100

    Removed kernel 6.5 (EOL)

--- Development/kernel/6.5/douninst.sh
+++
@@ -1,6 +0,0 @@
-# douninst.sh
-#
-# uninstall script for Slackware >= 15.0
-#
-# NOTE: This script is run AFTER package removal, so be careful!
-#       Consider it optional, use if it is really needed.
