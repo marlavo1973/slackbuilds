ca4990139ef6de33113852533be3375e4a8b1aa3
Marco Lavorini <marlavo1973@gmail.com>
2024-01-06 10:13:34 +0100

    Added kernel 6.6.10-zen1

---
+++ Development/kernel/6.6-zen/nvidia.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Install nvidia kernel modules
+#
+set -e
+
+export VERSION=6.6.10-zen1
+export NVIDIA=535.146.02
+export NUMJOBS=$(expr $(nproc) + 1)
+
+set -e
+
+if [ ! $UID = 0 ]; then
+  echo You must be 'root' to execute this script!
+  exit 1
+fi
+
+# install nvidia modules
+if [ -d /usr/src/nvidia-$NVIDIA ]; then
+  cd /usr/src/nvidia-$NVIDIA
+  KERNEL_UNAME=$VERSION make clean
+  KERNEL_UNAME=$VERSION make -j$NUMJOBS  modules
+  KERNEL_UNAME=$VERSION make -j$NUMJOBS  modules_install
+  depmod -a
+fi
